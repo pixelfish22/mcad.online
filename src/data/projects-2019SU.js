207c24eca990fa87430088fc7978c0ac75af8239
James Johnson <johnsojr@gmail.com>
2019-08-01 11:35:50 -0500

    added missing 2019su images

--- src/data/projects-2019SU.js
+++ src/data/projects-2019SU.js
@@ -4,8 +4,8 @@ export default [
     by: "Steve Carlson",
     description:
       "Traverse the gap between the world at large and the world in our hands.",
-    url: "https://scarlson973.studio.mcad.edu/Capstone/index.html"
-    // img: "img/magwd/2019SU/"
+    url: "https://scarlson973.studio.mcad.edu/Capstone/index.html",
+    img: "img/magwd/2019SU/scarlson973_thumbnail.png"
   },
   {
     title: "Thank You For Your Concern",
@@ -19,8 +19,8 @@ export default [
     by: "Brian Thue",
     description:
       "Shining a light on the true meaning of beauty to answer the question of how one lives a meaningful life.",
-    url: "https://brianpthue.github.io/capstone/"
-    // img: "img/magwd/2019SU/richardson_sara_thank-you.png"
+    url: "https://brianpthue.github.io/capstone/",
+    img: "img/magwd/2019SU/thue_brian_a_beautiful_threshold.png"
   },
   {
     title: "Surveil",
