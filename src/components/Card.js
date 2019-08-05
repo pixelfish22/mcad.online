53a01d7b3d2d9297ee5af7e2829411ce6a8d89dd
James Johnson <johnsojr@gmail.com>
2019-08-04 20:45:47 -0500

    Image hover style changes.
    Remove padding. Add rotation and scale.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,18 @@ import "./Card.css";
 
 export default ({ title, by, url, img, description }) => (
   <div className="card">
-    <a href={url} target="_blank" rel="noopener noreferrer">
-      {img ? (
-        <img src={img} alt={title} />
-      ) : (
-        <div className="img-placeholder" />
-      )}
+    <a href={url}>
+      <div className="card__img-container">
+        {img ? (
+          <img src={img} alt={title} />
+        ) : (
+          <div className="img-placeholder" />
+        )}
+      </div>
     </a>
     <div className="card-label">
       <h2 className="card-label__by">{by}</h2>
-      <a href={url} target="_blank" rel="noopener noreferrer">
+      <a href={url}>
         <p className="card-label__title">{title}</p>
       </a>
       <p className="card-label__description">{description}</p>
